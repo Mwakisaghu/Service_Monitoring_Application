7e2c4ff295d68baf59f460b8f5fbb89461829d93
Harrison <harrisonchambela2016@gmail.com>
2023-12-30 12:20:38 +0300

    This implements the create csv class logic, adds a function to print console output messages with the timestamp.

--- src/main/java/org/example/CreateCsvFile.java
+++ src/main/java/org/example/CreateCsvFile.java
@@ -1,6 +1,9 @@
 package org.example;
 
-import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Util.println;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class CreateCsvFile {
     public static void main(String[] args) {
@@ -21,6 +24,46 @@ public class CreateCsvFile {
                 "Seconds"};
 
         // Console Log - App starts
-        println("Application started");
+        print("Application started...");
+
+        // Create and write to the CSV file
+        try {
+            FileWriter csvWriter = new FileWriter(csvFilePath);
+
+            // Writing the header
+            writeDataRow(csvWriter, header);
+
+            // Writing data rows
+            writeDataRow(csvWriter, service1Data);
+            writeDataRow(csvWriter, service2Data);
+
+            // Close the writer
+            csvWriter.close();
+
+            // Print CSV creation
+            print("CSV file created successfully at: " + csvFilePath);
+
+        } catch (IOException e) {
+            System.err.println("Error occurred while creating the CSV file: " + e.getMessage());
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void writeDataRow(FileWriter csvWriter, String[] data) throws IOException {
+        // Write data items into the csv file
+        for (String dataItem : data) {
+            csvWriter.append(dataItem);
+            csvWriter.append(",");
+        }
+        csvWriter.append("\n");
+    }
+
+    private static void print(String message) {
+        // Print messages with timestamp - for the console output
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        String timestamp = dtf.format(now);
+        System.out.println("[" + timestamp + "] " + message);
     }
 }
