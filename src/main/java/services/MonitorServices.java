2a4fbeb589ea599a11b25cb7a9028a2c6042945a
Harrison <harrisonchambela2016@gmail.com>
2023-12-31 14:21:34 +0300

    This creates a service monitor class, loops through each service, and logs the status of the current service

--- src/main/java/services/MonitorServices.java
+++ src/main/java/services/MonitorServices.java
@@ -1,11 +1,17 @@
 package services;
 
+import org.example.CreateCsvFile;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class MonitorServices {
-    private String csvFilePath;
+    private final String csvFilePath;
 
     // Constructor - initialise the path
     public MonitorServices(String csvFilePath) {
@@ -20,5 +26,38 @@ public class MonitorServices {
 
         // Periodically monitor services in csv
         scheduledExecutorService.scheduleAtFixedRate(() -> serviceMonitor(csvFilePath), 0, 1, TimeUnit.MINUTES);
+    }
+
+    // Monitoring services - in the created csv file
+    private void serviceMonitor() throws FileNotFoundException {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(csvFilePath))) {
+            String line;
+
+            // Skips the csv header
+            bufferedReader.readLine();
+
+            // Looping through each service in the csv
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] serviceData = line.split(",");
+                String serviceName = serviceData[1];
+                String host = serviceData[2];
+                int port = Integer.parseInt(serviceData[3]);
+                String resourceUri = serviceData[4];
+
+                // Record/log status of the current service
+                logStatus(serviceName, host, port, resourceUri);
+            }
+
+        } catch (IOException e) {
+            // Handle errors when reading the csv file
+            CreateCsvFile.print("An error occurred while reading the csv file: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+
+    // Function to log statuses of each service
+    private void logStatus(String serviceName, String host, int port, String resourceUri) {
+
     }
 }
